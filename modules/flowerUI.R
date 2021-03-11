fa7f36499fbe2f6107cc449f230df2c84626e591
akostiv8 <nolik2000@yahoo.com>
2021-03-11 18:29:07 +0200

    Update flowerUI.R

--- modules/flowerUI.R
+++ modules/flowerUI.R
@@ -10,16 +10,17 @@ UIflower <- function(id){
           shiny.semantic::slider_input(ns("points_in"), min = 100, max = 10000, value = 1000, step = 300, class = "labeled ticked"),
           withSpinner(plotOutput(ns("cutomFlower")), color="#aba125")
       )
-    ),
-    fluidRow(
-      tabBox(title = "Math flower 'Golden Angle'", color = "black", width = 15,
-             collapsible = FALSE,
-             tabs = list(
-               list(menu = "Сhamomile", content = withSpinner(plotOutput(ns("flower_one")), color="#aba125")),
-               list(menu = "Dandelion", content = withSpinner(plotOutput(ns("flower_two")), color="#aba125")),
-               list(menu = "Sunflower", content = withSpinner(plotOutput(ns("flower_three")), color="#aba125"))
-             )),
     )
+    #,
+    #fluidRow(
+    #  tabBox(title = "Math flower 'Golden Angle'", color = "black", width = 15,
+     #        collapsible = FALSE,
+      #       tabs = list(
+       #        list(menu = "Сhamomile", content = withSpinner(plotOutput(ns("flower_one")), color="#aba125")),
+        #       list(menu = "Dandelion", content = withSpinner(plotOutput(ns("flower_two")), color="#aba125")),
+         #      list(menu = "Sunflower", content = withSpinner(plotOutput(ns("flower_three")), color="#aba125"))
+          #   )),
+    #)
     
     
     #tabItem(tabName = "math_tab",
