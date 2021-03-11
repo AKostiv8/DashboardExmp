ec7f631bf08e13d1e3bea4bd82b474258bd3bc46
akostiv8 <nolik2000@yahoo.com>
2021-03-11 18:43:42 +0200

    Update flowerUI.R

--- modules/flowerUI.R
+++ modules/flowerUI.R
@@ -5,7 +5,7 @@ UIflower <- function(id){
     fluidRow(
       box(title = "Custom flower", color = "black", width = 15,
         #  tags$label(class = "label_styl", textOutput(ns("angleVal"))),
-          shiny.semantic::slider_input(ns("angle_in"), min = 1, max = 45, value = 2, step = 1, class = "labeled ticked"),
+          slider_input(ns("angle_in"), min = 1, max = 45, value = 2, step = 1, class = "labeled ticked"),
         #  tags$label(class = "label_styl", textOutput(ns("pointsVal"))),
        #   shiny.semantic::slider_input(ns("points_in"), min = 100, max = 10000, value = 1000, step = 300, class = "labeled ticked"),
       #    withSpinner(plotOutput(ns("cutomFlower")), color="#aba125")
